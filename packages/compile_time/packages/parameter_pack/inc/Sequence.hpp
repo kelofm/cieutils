b0d97d6e20a8a6d07674399e098cba2f11271461
matekelemen <kelemen.mate.12@gmail.com>
2023-08-28 03:19:41 +0200

    add compile time integer sequences

---
+++ packages/compile_time/packages/parameter_pack/inc/Sequence.hpp
@@ -0,0 +1,44 @@
+#ifndef CIE_UTILS_COMPILE_TIME_SEQUENCE_HPP
+#define CIE_UTILS_COMPILE_TIME_SEQUENCE_HPP
+
+// --- STL Includes ---
+#include <bits/utility.h>
+#include <utility>
+
+
+namespace cie::ct {
+
+
+namespace impl {
+template <std::size_t, class>
+struct ReverseIndexSequence;
+
+template <std::size_t N, std::size_t ...Is>
+struct ReverseIndexSequence<N, std::index_sequence<Is...>>
+    : public std::index_sequence<(N - Is)...>
+{
+};
+} // namespace impl
+
+
+template <std::size_t N>
+using MakeIndexSequence = std::make_index_sequence<N>;
+
+
+template <std::size_t ...Is>
+using IndexSequence = std::index_sequence<Is...>;
+
+
+template <std::size_t N>
+requires (0 < N)
+using MakeReverseIndexSequence = impl::ReverseIndexSequence<N-1, std::make_index_sequence<N>>;
+
+
+template <std::size_t ...Is>
+using ReverseIndexSequence = IndexSequence<Is...>;
+
+
+} // namespace cie::ct
+
+
+#endif
