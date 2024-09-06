252b1165167bc38a2e447f78196887d6f1fc9ef8
matekelemen <kelemen.mate.12@gmail.com>
2024-09-06 10:37:02 +0200

    improve benchmark plots

--- python/modules/benchutils/graphics.py
+++ python/modules/benchutils/graphics.py
@@ -19,19 +19,18 @@ colors = {
 }
 
 
-def findMinPrefix(contents: list[Result], quantityName: str):
+def findMinPrefix(cases: list[Result.Case], quantityName: str):
     minPrefix = (None, None)
     prefixes = set()
 
-    if isinstance(contents, list):
-        for results in contents:
-            for case in results.cases:
-                prefixes.add(case[quantityName].getPrefix())
-    elif isinstance(contents, Result):
-        for case in contents.cases:
+    if isinstance(cases, list):
+        for case in cases:
+            prefixes.add(case[quantityName].getPrefix())
+    elif isinstance(cases, Result.Case):
+        for case in cases:
             prefixes.add(case[quantityName].getPrefix())
     else:
-        return ValueError(f"Unexpected input type: {type(contents)}")
+        return ValueError(f"Unexpected input type: {type(cases)}")
 
     for prefix in prefixes:
         exponent = Result.Case.Quantity.getPrefixMap()[prefix]
@@ -41,35 +40,34 @@ def findMinPrefix(contents: list[Result], quantityName: str):
     return minPrefix[0]
 
 
-def homogenizeUnits(results: Result, quantityName: str, commonPrefix: str = "") -> None:
+def homogenizeUnits(cases: list[Result.Case], quantityName: str, commonPrefix: str = "") -> None:
     if not commonPrefix:
-        commonPrefix = findMinPrefix(results, quantityName)
+        commonPrefix = findMinPrefix(cases, quantityName)
 
-    for case in results.cases:
+    for case in cases:
         case[quantityName].setPrefix(commonPrefix)
 
 
-def plotQuantity(results: Result,
-                 quantityName: str,
-                 axes: pyplot.Axes,
-                 color: list[float] = colors["TUMBlue"],
-                 textColor: list[float] = colors["black"],
-                 prefix: str = None,
-                 label: str = "",
-                 positions: numpy.array = None,
-                 barCount: int = 1,
-                 barIndex: int = 0,
-                 totalWidth: float = 0.8,
-                 format = R"{:.2E}") -> typing.Any:
+def addBars(cases: list[Result.Case],
+            quantityName: str,
+            axes: pyplot.Axes,
+            color: list[float] = colors["TUMBlue"],
+            prefix: str = None,
+            label: str = "",
+            positions: numpy.array = None,
+            barCount: int = 1,
+            barIndex: int = 0,
+            totalWidth: float = 0.8,
+            format = R"{:.2E}") -> typing.Any:
     if not prefix:
-        prefix = findMinPrefix(results, quantityName)
-    homogenizeUnits(results, quantityName, commonPrefix = prefix)
+        prefix = findMinPrefix(cases, quantityName)
+    homogenizeUnits(cases, quantityName, commonPrefix = prefix)
 
     if not label:
         label = quantityName
 
     # Collect values
-    values = [case[quantityName].getValue() for case in results.cases]
+    values = [case[quantityName].getValue() for case in cases]
 
     if positions is None:
         positions = numpy.arange(len(values))
@@ -88,53 +86,63 @@ def plotQuantity(results: Result,
         height = rectangle.get_height()
         width = rectangle.get_width()
         position = rectangle.get_x()
-        axes.text(
-            position + width / 2.0,
-            height * ((barIndex + 1) / barCount),
-            format.format(height),
-            ha = "center",
-            va = "bottom",
-            color = textColor,
-            fontsize = 14
-        )
+        textColor = [0.0, 0.0, 0.0, 1.0] if barIndex % 2 else [1.0, 1.0, 1.0, 1.0]
+        axes.text(position + width / 2.0,
+                  height * ((barIndex + 1) / barCount),
+                  format.format(height),
+                  ha = "center",
+                  va = "bottom",
+                  color = textColor,
+                  fontsize = 14)
 
+    axes.set_title(f"{quantityName} [{cases[0][quantityName].unit}]")
     return bars
 
 
 def plot(contents: list[Result],
-         quantityNames: list[str] = None,
+         quantityNames: list[str] = [],
          labelSizes: tuple[int,int] = (16, 16),
          labelColors: tuple[list[float],list[float]] = (colors["black"], colors["black"])) -> None:
+    # Find and group matching cases in the contents by name
+    groupedCases: dict[str,list[Result.Case]] = {}
+    caseNames: set[str] = [] # <== not built using a set to preserve original order
+    for results in contents:
+        for case in results.cases:
+            if not case.name in caseNames:
+                caseNames.append(case.name)
 
-    numberOfSubplots = len(contents)
+    for caseName in caseNames:
+        for results in contents:
+            case = next((c for c in results.cases if c.name == caseName), None)
+            groupedCases.setdefault(caseName, []).append(case)
+
+    numberOfSubplots = len(quantityNames)
     grid = (int(math.sqrt(numberOfSubplots)), 1)
     if grid[0]:
         grid = (grid[0], math.ceil(numberOfSubplots / grid[0]))
 
-    figure, axisSets = pyplot.subplots(grid[0], grid[1], num = "Benchmark Result")
+    figure, axisSets = pyplot.subplots(grid[0], grid[1], num = "Benchmark Results")
     if 1 < len(contents):
         axisSets = [axisSets]
     axisSets = numpy.ravel(axisSets)
 
     colorKeys = list(iter(colors.keys()))
-    for results, axes in zip(contents, axisSets):
-        labels = [case.name for case in results.cases]
-        positions = numpy.arange(len(labels))
-        for quantityIndex, quantityName in enumerate(quantityNames):
-            colorKey = colorKeys[quantityIndex % len(colorKeys)]
+    labels = caseNames
+    positions = numpy.arange(len(caseNames))
+    for quantityName, axes in zip(quantityNames, axisSets):
+        for i_result in range(len(contents)):
+            colorKey = colorKeys[i_result % len(colorKeys)]
             color = colors[colorKey]
             textColor = colors["gray"] if colorKey == "black" else colors["black"]
-            graph = plotQuantity(
-                results,
-                quantityName,
-                axes,
-                barCount = len(quantityNames),
-                barIndex = quantityIndex,
-                positions = positions,
-                color = color,
-                textColor = textColor)
+            graph = addBars([cases[i_result] for cases in groupedCases.values()],
+                            quantityName,
+                            axes,
+                            barCount = len(contents),
+                            barIndex = i_result,
+                            positions = positions,
+                            color = color)
         axes.set_xticks(positions)
-        axes.set_xticklabels(labels, rotation = 45, ha = "right")
+        axes.set_xticklabels(caseNames, rotation = 45, ha = "right")
 
         # Set y axis scale
         yRange = [float("inf"), float("-inf")]
